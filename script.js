aadd21d9527343930a17a134158a1a6c46223011
Youstina Morgan <Youstina.morgan1995@gmail.com>
2024-07-21 21:04:17 +0300

    advanced 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const addButton = document.getElementById('add-task-btn');
-    const taskinput = document.getElementById('task-input');
-    const tasklist = document.getElementById('task-list');
+    const addButton = document.getElementById('add task');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
 
     function addTask() {
         const task = taskInput.value.trim();
