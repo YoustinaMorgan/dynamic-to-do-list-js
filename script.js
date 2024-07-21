d5c5f82202eb0b7b3a14798a512f715e30910cc3
Youstina Morgan <Youstina.morgan1995@gmail.com>
2024-07-21 20:46:41 +0300

    advanced 1

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.getElementById('add-task-btn');
+    const taskinput = document.getElementById('task-input');
+    const tasklist = document.getElementById('task-list');
+
+    function addTask() {
+        const task = taskInput.value.trim();
+        if (taskText==="") {
+            alert("please enter a task.");
+            return;
+        }
+    const li = document.createElementNS('li');
+    li.textContent = taslText;
+
+    const removeButton = document.createElement('button');
+    removeButton.textcontent = "Remove";
+    removeButton.className = 'remove.btn';
+
+    removeButton.onclick = function() {
+        tasklist.removeChild(li);
+    };
+
+    li.appendChild(removeButton);
+    taskiList.appendChild(li);
+
+    taskInput.value = "";
+    }
+
+    addButton.addEventListener('click', addTask);
+
+    taskInput.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    });
+    addTask();
+});
